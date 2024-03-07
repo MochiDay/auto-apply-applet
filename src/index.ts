921aaa5c5b2bc7dc46ef68e567d111d90bd53bbb
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-03-06 21:43:00 -0800

    feat: set up lever apply api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response, Application } from "express";
 
 const app: Application = express();
+app.use(express.json());
+
 const port = 8762;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+/**
+ * @route POST /api/apply/lever
+ * @desc Apply to Lever job postings
+ * @access Public
+ *
+ * @param job_urls - Array of job URLs to apply to. For Lever job postings,
+ * the URL should follow the format:
+ * https://jobs.lever.co/{company}/{job_id}/apply
+ */
+app.post("/api/apply/lever", (req: Request, res: Response) => {
+  const body = req.body;
+  const { job_urls } = body;
+
+  res.send(job_urls);
 });
 
 app.listen(port, () => {
